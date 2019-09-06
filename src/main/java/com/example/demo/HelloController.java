9f267e59c3e025e9e2d2a68465a291ac265dbf8d
Vance <vance.ho@cherricorp.tech>
2019-09-06 22:34:12 +0800

    hello-2

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,4 +1,15 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
 public class HelloController {
+
+    @RequestMapping("/")
+    public String sayHello() {
+        System.out.println("hello");
+        return "hello";
+    }
 }
